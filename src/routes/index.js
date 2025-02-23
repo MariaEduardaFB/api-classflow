745f99e249660aa9d3190ae60981fb95a8b50250
mw-bl <bezerram533@gmail.com>
2025-02-23 18:42:45 -0300

    chamando rotas de horarios

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { alunoRoutes } from './aluno.js';
 import { disciplinaRoutes } from './disciplina.js';
 import { projetoRoutes } from './projeto.js';
+import { horarioRoutes } from './horario.js';
 
 export const router = express.Router();
 
@@ -9,3 +10,4 @@ export const router = express.Router();
 alunoRoutes(router);
 disciplinaRoutes(router);
 projetoRoutes(router);
+horarioRoutes(router);
