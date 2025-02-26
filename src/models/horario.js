175d71e18d866f1749c38b19879e2b46a915e943
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2025-02-26 08:28:32 -0300

    Corrigindo sintaxe

--- src/models/horario.js
+++ src/models/horario.js
@@ -6,13 +6,33 @@ import { Disciplina } from './disciplina.js';
 export const Horario = sequelize.define(
   'Horario',
   {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    cargaHoraria: { type: DataTypes.INTEGER, allowNull: false },
-    diaSemana: { type: DataTypes.STRING(20), allowNull: false },
-    hInicio: { type: DataTypes.TIME, allowNull: false },
-    hFim: { type: DataTypes.TIME, allowNull: false },
-    alunoId: { type: DataTypes.INTEGER, allowNull: false },
-    disciplinaId: { type: DataTypes.INTEGER, allowNull: false },
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
+    cargaHoraria: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false 
+    },
+    diaSemana: { 
+      type: DataTypes.STRING(20), 
+      allowNull: false 
+    },
+    hInicio: { 
+      type: DataTypes.TIME, 
+      allowNull: false 
+    },
+    hFim: { 
+      type: DataTypes.TIME, 
+      allowNull: false 
+    },
+    alunoId: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false },
+    disciplinaId: { 
+      type: DataTypes.INTEGER, 
+      allowNull: false },
   },
   {
     tableName: 'horarios',
