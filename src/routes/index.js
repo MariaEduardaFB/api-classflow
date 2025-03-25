130bcf86a5963997e2555b17aa635b20b5c2fe0c
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2025-03-25 14:21:50 -0300

    :sparkles: feat: Adicionando chamada das rotas/model de avaliações

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { alunoRoutes } from './aluno.js';
+import { avaliacaoRoutes } from './avaliacao.js';
 import { disciplinaRoutes } from './disciplina.js';
 import { projetoRoutes } from './projeto.js';
 import { horarioRoutes } from './horario.js';
@@ -7,8 +8,8 @@ import { documentoRoutes } from './documentos.js';
 
 export const router = express.Router();
 
-
 alunoRoutes(router);
+avaliacaoRoutes(router);
 disciplinaRoutes(router);
 projetoRoutes(router);
 horarioRoutes(router);
